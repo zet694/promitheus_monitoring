8a3c798152b4bfe96cee49d3164a4127f6db94b3
Vitaliy Vrublevskiy <vitaliy@particle.one>
2020-06-27 13:47:49 +0300

    Refactor

--- prometheus/conf/docker-entrypoint.sh
+++ prometheus/conf/docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh -e
 
 cat /etc/prometheus/prometheus.yml > /tmp/prometheus.yml
-cat /etc/prometheus/weave-cortex.yml | \
-    sed "s@#password: <token>#@password: '$WEAVE_TOKEN'@g" > /tmp/weave-cortex.yml
-
-#JOBS=mongo-exporter:9111 redis-exporter:9112
 
 if [ ${JOBS+x} ]; then
 
@@ -25,22 +21,11 @@ cat >>/tmp/prometheus.yml <<EOF
       port: ${PORT}
 EOF
 
-cat >>/tmp/weave-cortex.yml <<EOF
-
-  - job_name: '${SERVICE}'
-    dns_sd_configs:
-    - names:
-      - 'tasks.${SERVICE}'
-      type: 'A'
-      port: ${PORT}
-EOF
-
 done
 
 fi
 
 mv /tmp/prometheus.yml /etc/prometheus/prometheus.yml
-mv /tmp/weave-cortex.yml /etc/prometheus/weave-cortex.yml
 
 set -- /bin/prometheus "$@"
 
